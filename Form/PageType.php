c2a935dcc563b4556180d8369ded6553fbe14981
mehmetsoylucom <mehmet@soylu.net>
2017-09-08 03:44:13 +0300

    Translation fix

--- Form/PageType.php
+++ Form/PageType.php
@@ -23,21 +23,16 @@ class PageType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $buttonName = $options['data']->getId() > 0 ? 'Duzenle' : 'Ekle';
+        $buttonName = $options['data']->getId() > 0 ? 'Edit' : 'Add';
 
         $builder
-            ->add('title', TextType::class, ['label' => 'Sayfa Adı', 'required' => true])
+            ->add('title', TextType::class, ['label' => 'Page Name', 'required' => true])
             ->add('meta_title', TextType::class, ['label' => 'Meta Title', 'required' => true])
             ->add('meta_keywords', TextType::class, ['label' => 'Meta Keywords', 'required' => true])
             ->add('meta_description', TextareaType::class, ['label' => 'Meta Description', 'required' => true])
-            ->add('slug', TextType::class, ['label' => 'Bağlantı (slug)', 'required' => true])
-            ->add('content', TextareaType::class, ['label' => 'Sayfa İçeriği', 'required' => false, 'attr' => ['class' => 'ckeditor']])
-            /*        ->add('user', EntityType::class,
-                    array(
-                        'class' => 'Basefony\UserBundle\Entity\User',
-                        'label' => 'Kullanici'
-                    ))
-                    */
+            ->add('slug', TextType::class, ['label' => 'URL Slug', 'required' => true])
+            ->add('content', TextareaType::class,
+                ['label' => 'Content', 'required' => false, 'attr' => ['class' => 'ckeditor']])
             ->add('user', Select2EntityType::class, [
                 'multiple' => false,
                 'remote_route' => 'basefony_admin_user_search_ajax',
@@ -51,7 +46,7 @@ class PageType extends AbstractType
                 'cache' => true,
                 'cache_timeout' => 60000,
                 'language' => 'tr',
-                'placeholder' => 'Kullanici Secin',
+                'placeholder' => 'User',
             ])
             ->add('submit', SubmitType::class, ['label' => $buttonName, 'attr' => ['class' => 'btn btn-primary']]);
 
